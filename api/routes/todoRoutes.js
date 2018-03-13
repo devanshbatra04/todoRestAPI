50deabf1989ea6810093e2caa4f8ed696394f7c9
hulkmaniac99 <devanshbatra04@gmail.com>
2018-03-13 23:32:51 +0530

    models and routes adde

---
+++ api/routes/todoRoutes.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+    var todoList = require('../controller/todoController');
+
+    //routes
+    app.route('/tasks')
+        .get(todoList.List)
+        .post(todoList.Create);
+
+
+    app.route('/tasks/:taskId')
+        .get(todoList.Read)
+        .put(todoList.Update)
+        .delete(todoList.Delete);
+
+};
